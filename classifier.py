b607b7cb82f680954dbfed829c6c1b8ebe5e10a9
Aerin Brown <72281505+a-brown-code@users.noreply.github.com>
2022-10-02 09:06:11 -0400

    refactor: joined pickling and model training

--- classifier.py
+++ classifier.py
@@ -3,18 +3,15 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn import metrics
 import pickle
 
-""" (list, list) -> KNeighborsClassifier
-Returns a trained KNN model on the list of floats (similarities) and their labels (strings). 
+""" (list, list) -> NoneType
+Pickles a trained KNN model on the list of similarities (floats) and their labels (ints) into model.pkl. 
 """
 def train_model(similarities, labels):
-    label_encoder = preprocessing.LabelEncoder() # not used if numeric_labels is not used
-    
-    #may not be necessary, only if following is given as a non-number
-    # numeric_labels = label_encoder.fit_transform(labels)
-
     model = KNeighborsClassifier(n_neighbors=5)
     model.fit(similarities, labels)
-    return model
+
+    with open("model.pkl", "wb") as model_pickle:
+        pickle.dump(model_pickle, model)
 
 """ (KNeighborsClassifier, list, list) -> float
 Returns the accuracy of the model's predictions for the given float similarities and strings labels.
@@ -28,11 +25,3 @@ Returns the integer representation of whether or not the pair is likely to follo
 """  
 def make_prediction(model, similarity):
     return model.predict([similarity])
-<<<<<<< HEAD
-=======
-
-model = train_model([[1.1, 5.2]], [[1, 0]]) # sample values for now
-
-# Save model to disk
-pickle.dump(model, open('model.pkl','wb'))
->>>>>>> flask-webapp
