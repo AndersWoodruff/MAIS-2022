d26d3df547223b97a2d06cdd7e72c2a84f48f4a2
AndersWoodruff <113076762+AndersWoodruff@users.noreply.github.com>
2022-10-02 11:13:03 -0400

    Newest scrapertest

--- scraperTest.py
+++ scraperTest.py
@@ -4,27 +4,30 @@ import pandas as pd
 import csv
 ###(sample,tweets,tweetstats)
 all_users = []
-"""with open("data.csv") as csvfile:
+with open("data.csv") as csvfile:
     reader = csv.reader(csvfile) # change contents to floats
     for row in reader:
-        all_users.append(row[1])"""
+        all_users.append(row[1])
 
 # Creating list to append tweet data to
-tweets_all=np.zeros((4,100,2),dtype=object,order="C")
-print(tweets_all)
+tweets_all=np.zeros((10000,101),dtype=object,order="C")
+
 def scrape_twitter(name,num):
     # Using TwitterSearchScraper to scrape data and append tweets to list
     global tweets_all
+    tweets_all[num-1][0]=name
     for i,tweet in enumerate(sntwitter.TwitterSearchScraper('from:{name}').get_items()):
-        if i>100 or tweet.content=="":
+        try:
+            tweets=tweet.rawContent
+        except:
+            pass
+        if i>99 or tweets=="":
             break
-        tweets_all[num-1,][i-1]=([tweet.content, tweet.user.username])
+        tweets_all[num-1][i+1]=([tweets])
 
-for num,name in enumerate(['Mixed_Lib']):
+for num,name in enumerate(all_users):
+    if num>200:
+        break
     scrape_twitter(name,num)
 
-print(tweets_all)
-# Creating a dataframe from the tweets list above 
-#tweets_df1 = pd.DataFrame(tweets_list1, columns=[ 'Text', 'Username'])
-
-#tweets_df1.to_csv(r'C:\juliawright\coding\friendFinder\my_data.csv', index=False)
+np.save("All-tweets-array-2",tweets_all, allow_pickle=True)
